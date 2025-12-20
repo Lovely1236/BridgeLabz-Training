575e784a5093decc993297ce6af8682c894bf456
lovely1236 <mycodingy@gmail.com>
2025-12-20 14:51:15 +0530

    (Lovely Pandey)Refactor. Added comments for Counting down number using while loop

--- core-java-practice/gcr-codebase/control-flow/CountdownNumber.java
+++ core-java-practice/gcr-codebase/control-flow/CountdownNumber.java
@@ -8,10 +8,13 @@ public class CountdownNumber {
         int counter = sc.nextInt();
 
         while(counter == 1){
+            // If counter is 1, just print 1 and break
             System.out.println(counter);
             break;
         }
+       
 
+        // if counter is greater than 1
         while(counter > 1){
             System.out.println(counter);
             counter--;

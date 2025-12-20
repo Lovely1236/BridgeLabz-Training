b179435b44330cbcd06f4df821da4d1e20144c8c
lovely1236 <mycodingy@gmail.com>
2025-12-20 14:54:46 +0530

    (Lovely Pandey)Refactor. Added comments for Number count down using for loop

--- core-java-practice/gcr-codebase/control-flow/CountNumberFor.java
+++ core-java-practice/gcr-codebase/control-flow/CountNumberFor.java
@@ -5,7 +5,9 @@ public class CountNumberFor {
        Scanner sc = new Scanner(System.in);
 
        int counter= sc.nextInt();
+         
 
+       // Using for loop to count down from counter to 1
        for (int i = counter; i >= 1; i--) {
            System.out.println(i);
        }

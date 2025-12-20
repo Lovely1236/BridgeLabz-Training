c86278c25d4ccf12c9ebfcc993a6117563c96178
lovely1236 <mycodingy@gmail.com>
2025-12-20 14:58:49 +0530

    (Lovely Pandey)Refactor. Added comments for Number divisible by 5

--- core-java-practice/gcr-codebase/control-flow/DivisibleByFive.java
+++ core-java-practice/gcr-codebase/control-flow/DivisibleByFive.java
@@ -4,12 +4,14 @@ public class DivisibleByFive {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
 
+
+        // Check if the number is divisible by 5
         if (number % 5 == 0) {
 
             System.out.println(" Is the number " + number + " divisible by 5? Yes");
 
         } else {
-
+// If not divisible by 5
             System.out.println(" Is the number " + number + " divisible by 5? No");
         }
         sc.close();

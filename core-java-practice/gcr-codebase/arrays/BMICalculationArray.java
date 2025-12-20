9bb56d2b85b0f1083272f5e8976420753165ce2c
lovely1236 <mycodingy@gmail.com>
2025-12-20 23:49:32 +0530

    (Lovely Pandey)Add. Calculating BMI Of Person using Array

--- core-java-practice/gcr-codebase/arrays/BMICalculationArray.java
+++ core-java-practice/gcr-codebase/arrays/BMICalculationArray.java
@@ -8,6 +8,8 @@ public class BMICalculationArray {
         
         double[] weights = new double[numPeople];
         double[] heights = new double[numPeople];
+         double[] bmi = new double[numPeople];
+        String[] status = new String[numPeople];
         
         for (int i = 0; i < numPeople; i++) {
             System.out.print("Enter weight in pounds for person " + (i + 1) + ": ");
@@ -18,9 +20,22 @@ public class BMICalculationArray {
         }
         
         System.out.println("BMI values for each person:");
+        for (int i = 0; i < n; i++) {
+            bmi[i] = weight[i] / (height[i] * height[i]);
+
+            if (bmi[i] <= 18.4) {
+                status[i] = "Underweight";
+            } else if (bmi[i] <= 24.9) {
+                status[i] = "Normal";
+            } else if (bmi[i] <= 39.9) {
+                status[i] = "Overweight";
+            } else {
+                status[i] = "Obese";
+            }
+        }
+
         for (int i = 0; i < numPeople; i++) {
-            double bmi = (weights[i] / (heights[i] * heights[i])) * 703;
-            System.out.println("Person " + (i + 1) + ": " + bmi);
+            System.out.println("Person " + (i + 1) + ": BMI = " + bmi[i] + ", Status = " + status[i]);
         }
     }
 }

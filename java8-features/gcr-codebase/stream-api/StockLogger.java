527b29472136fb2d52bafaeeb5c028b3dcd3d014
lovely1236 <mycodingy@gmail.com>
2026-02-09 10:43:10 +0530

    (Lovely Pandey)Add. Stream API questions

---
+++ java8-features/gcr-codebase/stream-api/StockLogger.java
@@ -0,0 +1,10 @@
+package streamApi;
+import java.util.*;
+
+public class StockLogger {
+    public static void main(String[] args){
+        List<Double> prices=List.of(102.5,104.2,101.8,105.6);
+
+        prices.forEach(p->System.out.println("Stock Price:"+p));
+    }
+}

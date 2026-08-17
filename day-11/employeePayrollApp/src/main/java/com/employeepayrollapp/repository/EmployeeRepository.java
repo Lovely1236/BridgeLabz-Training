5cc1f78f94f89a2529dbf5eb2fcf8e8367c1fcfd
lovely1236 <mycodingy@gmail.com>
2026-08-17 13:29:57 +0530

    (Lovely Pandey)Add. day-11 spring data jpa

---
+++ day-11/employeePayrollApp/src/main/java/com/employeepayrollapp/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.employeepayrollapp.repository;
+
+import com.employeepayrollapp.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ day-11/employeePayrollApp/src/main/java/com/employeepayrollapp/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.employeepayrollapp.service;
+
+import com.employeepayrollapp.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+
+    void deleteEmployee(Long id);
+}
